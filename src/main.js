8e0acfbb34982ae831c6dbbc7268f73021680b23
shiran <szelnir@gmail.com>
2018-07-25 17:39:39 +0300

    vuetify

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import '../public/scss/main.css'
 import axios from 'axios'
+import Vuetify from 'vuetify'
+
 import './registerServiceWorker';
+
+import '../public/scss/main.css'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(Vuetify)
 axios.defaults.withCredentials = true;
 Vue.config.productionTip = false;
 
